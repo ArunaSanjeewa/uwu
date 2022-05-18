b5ed51d920430eb10c39f02b24320ac3398b505c
aruna.sanjeewa@eimsky.com <aruna.sanjeewa@eimsky.com>
2022-05-18 10:08:40 +0530

    all4

---
+++ build/precache-manifest.6c0d0a24e9dcd517b8f9f8f35b743147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e541435a4638de23155b5f12421110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5bffb7b830cfd0d333",
+    "url": "/static/css/2.f7375132.chunk.css"
+  },
+  {
+    "revision": "25ed00fba0b1d8b09f35",
+    "url": "/static/css/main.4c7be86a.chunk.css"
+  },
+  {
+    "revision": "da5bffb7b830cfd0d333",
+    "url": "/static/js/2.3f16514b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.3f16514b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ed00fba0b1d8b09f35",
+    "url": "/static/js/main.25199965.chunk.js"
+  },
+  {
+    "revision": "ace5e4fd5a39af15c51c",
+    "url": "/static/js/runtime-main.e76888ab.js"
+  }
+]);
