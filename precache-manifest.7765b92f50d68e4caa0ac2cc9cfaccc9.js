d53af243173c4b3bf39513f89043ee0fe8d4baea
aruna.sanjeewa@eimsky.com <aruna.sanjeewa@eimsky.com>
2022-05-18 10:13:21 +0530

    Updates

---
+++ precache-manifest.7765b92f50d68e4caa0ac2cc9cfaccc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2774e70db7a6710c7e14240f31dd8a51",
+    "url": "/uwu/index.html"
+  },
+  {
+    "revision": "e40f19aa009e8895d0d4",
+    "url": "/uwu/static/css/2.f7375132.chunk.css"
+  },
+  {
+    "revision": "25ed00fba0b1d8b09f35",
+    "url": "/uwu/static/css/main.4c7be86a.chunk.css"
+  },
+  {
+    "revision": "e40f19aa009e8895d0d4",
+    "url": "/uwu/static/js/2.25bbd87d.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/uwu/static/js/2.25bbd87d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ed00fba0b1d8b09f35",
+    "url": "/uwu/static/js/main.25199965.chunk.js"
+  },
+  {
+    "revision": "63dce2d9a953597dc383",
+    "url": "/uwu/static/js/runtime-main.7d180044.js"
+  }
+]);
